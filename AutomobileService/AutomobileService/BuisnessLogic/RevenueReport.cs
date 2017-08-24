af9b5c49a6ae40290f1cbcd1331c13db9008db49
Himanshu Soni <hsoni@tavisca.com>
2017-08-24 15:07:37 +0530

    added features support

---
+++ AutomobileService/AutomobileService/BuisnessLogic/RevenueReport.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutomobileService
+{
+    public class RevenueReport : IReport
+    {
+        private double _revenue=0;
+        public RevenueReport(double _revenue)
+        {
+            _revenue++;
+        }
+        double Revenue => _revenue;
+
+        public override string ToString()
+        {
+            return $"Revenue: {_revenue}";
+        }
+    }
+}
---
+++ AutomobileService/AutomobileService/BuisnessLogic/VehicleTagManager.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutomobileService
+{
+    public class VehicleTagManager
+    {
+        private Dictionary<int, VehicleTag> _onRentVehicles;
+        private Dictionary<int, VehicleTag> _onSellVehicles; 
+
+        public VehicleTagManager()
+        {
+            _onRentVehicles = new Dictionary<int, VehicleTag>();
+            _onSellVehicles = new Dictionary<int, VehicleTag>();
+        }
+
+        public Dictionary<int, VehicleTag> OnRentVehicles => _onRentVehicles;
+        public Dictionary<int, VehicleTag> OnSellVehicles => _onSellVehicles;
+
+        public void SetTag(int code, VehicleTag tag)
+        {
+            if (tag == VehicleTag.OnRent)
+                _onRentVehicles.Add(code, tag);
+            else
+                _onSellVehicles.Add(code, tag);                
+        }
+    }
+}
